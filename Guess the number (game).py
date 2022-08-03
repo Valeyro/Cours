33515d987b16240026df3aaf27f98e0e020aecd6
Valeyro <baselto.corentin@gmail.com>
2022-08-03 18:49:15 +0200

    Modification

--- Guess the number (game).py
+++ Guess the number (game).py
@@ -19,25 +19,24 @@ print(random_number)
 a= 0
 
 while a != random_number:
-    a=int(input("Enter the guessed value: "))
-        
-    if a>100 or a<0:
-            print("the random value is only between 0 and 100, please put a value in this interval")
-            
-    elif random_number - 2 < a < random_number+2:
-        if a<random_number:
-                print("the random number is higher than the value you entered but you are close")
-        elif a>random_number:
-               print("the random number is lower than the value you entered but you are close")
-    
-    elif a<random_number:
-                print("the random number is higher than the value you entered")
-        
-    elif a>random_number:
-               print("the random number is lower than the value you entered")
+        a=int(input("Enter the guessed value: "))
 
-    else:
-        print("Well done you found the exact value!")
+        if a>100 or a<0:
+                print("the random value is only between 0 and 100, please put a value in this interval")
+                
+        elif random_number - 2 < a < random_number+2:
+                if a<random_number:
+                        print("the random number is higher than the value you entered but you are close")
+                elif a>random_number:
+                        print("the random number is lower than the value you entered but you are close")
+
+        elif a<random_number:
+                print("the random number is higher than the value you entered")
+        elif a>random_number:
+                print("the random number is lower than the value you entered")
+        if a == random_number:
+                print("Well done you found the right number!")
+
 
 
 
